8fa66dad4a5c62d249dbece5ad327b82e5344b2f
Estrada44 <jesus@mexiconow.mx>
2021-02-22 15:33:01 -0700

    Animacion

---
+++ index.js
@@ -0,0 +1,29 @@
+let animateHTML = function() {
+    let elems,
+        windowHeight
+    
+    let init = () => {
+      elems = document.getElementsByClassName("hidden");
+      windowHeight = window.innerHeight;
+      _addEventHandlers();
+    }
+    
+    const _addEventHandlers = () => {
+        window.addEventListener("scroll", _checkPosition);
+        window.addEventListener("resize", init)
+    }
+    let _checkPosition = () => {
+      for ( let i = 0; i < elems.length; i++ ) {
+        let posFromTop = elems[i].getBoundingClientRect().top;
+        if ( posFromTop - windowHeight <= 0) { 
+          elems[i].className = elems[i].className.replace( "hidden", "fade-in" );
+        }
+      }    
+    }
+    
+    return {
+      init: init
+    }
+  }
+  
+  animateHTML().init();
